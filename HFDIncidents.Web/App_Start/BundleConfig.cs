bf1d4ac1b7470913721dbc55819a0e66b796c633
d-m-wilson <t00lfan@yahoo.com>
2015-04-12 18:56:27 -0500

    Added Yeti theme from bootswatch.com/yeti/

--- HFDIncidents.Web/App_Start/BundleConfig.cs
+++ HFDIncidents.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace HFDIncidents.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
