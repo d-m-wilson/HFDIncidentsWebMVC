f24a8043ed51f74f7ab81080af38f3211bf8a99c
d-m-wilson <t00lfan@yahoo.com>
2015-04-11 22:49:25 -0500

    Added HFDIncidentsReadOnlyContext, an EF DbContext-derived class which provides access to entities that is read-only and without change tracking

---
+++ HFDActiveIncidents.Domain/Models/HFDIncidentsReadOnlyContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HFDActiveIncidents.Domain.Models
+{
+    public class HFDIncidentsReadOnlyContext : DbContext, IIncidentDataSource
+    {
+        public HFDIncidentsReadOnlyContext()
+            : base("Name=HFDActiveIncidentsReadOnlyContext")
+        {
+            System.Data.Entity.Database.SetInitializer<Models.HFDIncidentsReadOnlyContext>(null);
+        }
+
+        System.Linq.IQueryable<ArchivedIncident> IIncidentDataSource.ArchivedIncidents
+        {
+            get
+            {
+                return Set<ArchivedIncident>()
+                    .AsNoTracking()
+                    .Include(ai => ai.IncidentType.Agency);
+            }
+        }
+
+        System.Linq.IQueryable<IncidentType> IIncidentDataSource.IncidentTypes
+        {
+            get 
+            {
+                return Set<IncidentType>()
+                    .AsNoTracking()
+                    .Include(it => it.Agency);
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            throw new InvalidOperationException("This context is read-only.");
+        }
+
+        public override System.Threading.Tasks.Task<int> SaveChangesAsync()
+        {
+            throw new InvalidOperationException("This context is read-only.");
+        }
+
+        public override System.Threading.Tasks.Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            throw new InvalidOperationException("This context is read-only.");
+        }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ArchivedIncident>();
+            modelBuilder.Entity<IncidentType>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
